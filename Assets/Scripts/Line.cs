9912e47dd1473953b0c74d61ea65c29472204d08
DmitryKotusev <dssa-1966@yandex.ru>
2019-05-21 13:25:36 +0300

    Init commit.

---
+++ Assets/Scripts/Line.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+[Serializable]
+public class Line
+{
+    public Transform point1;
+    public Transform point2;
+}
---
+++ Assets/Scripts/LineDrawer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineDrawer : MonoBehaviour
+{
+    public GameObject linesContainer;
+    public GameObject linePrefab;
+
+    public Line[] lines;
+
+    void DrawLines()
+    {
+        foreach (Line line in lines)
+        {
+            Vector3 globalRespawnPosition = (line.point1.position + line.point2.position) / 2f;
+            GameObject newPoint = Instantiate(linePrefab, globalRespawnPosition, linePrefab.transform.rotation, linesContainer.transform);
+            LineRenderer lineRenderer = newPoint.GetComponent<LineRenderer>();
+            lineRenderer.positionCount = 2;
+            lineRenderer.SetPositions(new Vector3[] { newPoint.transform.InverseTransformPoint(line.point1.position),
+                newPoint.transform.InverseTransformPoint(line.point2.position) });
+        }
+    }
+
+    private void Start()
+    {
+        DrawLines();
+    }
+}
